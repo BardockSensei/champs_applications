68448a73e3686f00e0dd21669d6331e5cd40b5f1
Guillaume Hostache <guillaume.hostache@outlook.fr>
2024-11-06 09:41:51 +0100

    tp1: première partie

---
+++ tp1-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Séance n°1';
+  description =
+    'Prise en main d’Angular, première application, liaisons des données';
+}
